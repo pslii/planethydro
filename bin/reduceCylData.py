ea8cf135633656b692539cd36e98de7f65ae39c9
pslii <mromanova@astro.cornell.edu>
2014-12-02 17:07:55 -0500

    Added option to specify start and end ndats.

--- bin/reduceCylData.py
+++ bin/reduceCylData.py
@@ -8,8 +8,18 @@ def main(argv):
     try:
         opts, args = getopt.getopt(argv, 'h', longopts=['x', 'xz', 'xy', 'xyz', 'time', 'png', 'all'])
     except getopt.GetoptError:
-        print "reduceCylData --all --x --xy --xz --xyz --png --time"
+        print "reduceCylData --all --x --xy --xz --xyz --png --time [start] [end]"
         sys.exit(2)
+
+    start, end = None, None
+    if len(args) == 1:
+        start = int(args[0])
+    elif len(args) == 2:
+        start = int(args[0])
+        end = int(args[1])
+    elif len(args) >= 3:
+        print "Usage: reduceCylData --all --x --xy --xz --xyz --png --time [start] [end]"
+
     output = []
     for opt, arg in opts:
         if opt == '-h':
@@ -32,7 +42,7 @@ def main(argv):
             output = ['x', 'xz', 'xy', 'xyz', 'time', 'png']
     if len(output) == 0:
         output = ['x', 'xy', 'time', 'png']
-    processCylData(output, verbose=False)
+    processCylData(output, verbose=False, datarange=(start, end))
 
 
 if __name__ == "__main__":
